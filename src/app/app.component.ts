21807936ee5f871a799b46841a21fc63f44d5cb0
Vitaly Bobrov <vitalybobrov@mail.ru>
2021-06-06 12:09:09 +0400

    moved menu in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {ISection} from './interfaces/interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  title = 'need-for-drive';
+  public sections: ISection[] | undefined;
+
+  constructor(public translate: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    this.translate.addLangs(['ru', 'en']);
+    this.translate.setDefaultLang('ru');
+    this.sections = [
+      {
+        value: 'parking',
+        title: '',
+      },
+      {
+        value: 'insurance',
+        title: '',
+      },
+      {
+        value: 'petrol',
+        title: '',
+      },
+      {
+        value: 'service',
+        title: '',
+      },
+    ];
+    this.translate.setDefaultLang('ru');
+    this.translate.stream('MAIN PAGE.menu sections').subscribe(sections => {
+      this.sections.map(section => {
+        section.title = sections[section.value].title;
+      });
+    });
+  }
 }
